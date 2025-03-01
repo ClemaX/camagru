ab74a01a0582dac4791acabed0c35e20059e6060
Clément Hamada <clement.hamada@elysium-security.com>
2025-03-01 22:36:57 +0100

    fix(views): add password field validation pattern

--- src/Views/signup.php
+++ src/Views/signup.php
@@ -34,9 +34,9 @@
 	<div class="form-group">
 		<label for="password">Password</label>
 		<input type="password" class="form-control" id="password" name="password" required
-					 minlength="8">
+					 pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\-+_!@#$%^&*., ?]).{8,}$" maxlength="254">
 		<div class="invalid-feedback">
-			Password must be at least 8 characters long.
+			Password must be at least 8 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character.
 		</div>
 	</div>
 
