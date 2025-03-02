f576a61e0e0e3c11d7eea70246e1d4ef2afd7d18
Clément Hamada <clement.hamada@elysium-security.com>
2025-03-02 14:58:10 +0100

    fix: use correct constructor parameters in auth exception

--- src/Exceptions/AuthException.php
+++ src/Exceptions/AuthException.php
@@ -6,6 +6,6 @@ class AuthException extends HttpException
 {
     public function __construct(string $message, int $code = 4010)
     {
-        parent::__construct(401, $message, $code);
+        parent::__construct(401, "Authentication Failed", $message, $code);
     }
 }
