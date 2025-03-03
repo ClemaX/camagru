ec4f62a08bed13aada1122f5fb9b434bdcedb6dc
Clément Hamada <clement.hamada@elysium-security.com>
2025-03-03 23:20:28 +0100

    feat(ui): add cards for login and signup

--- src/Views/login.php
+++ src/Views/login.php
@@ -1,35 +1,40 @@
-<h1>Log In</h1>
+<div class="card">
+	<div class="card-header">
+		<h1 class="card-title">Log In</h1>
+	</div>
+	<div class="card-body">
+		<form class="needs-validation d-flex flex-column gap-3" novalidate method="post">
+			@csrf
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input type="text" class="form-control"
+							id="username" name="username" required
+							value="{{ $username }}">
+				<div class="invalid-feedback">
+					Username is required.
+				</div>
+			</div>
 
-@if($errorMessage !== null)
-<div class="alert alert-danger mt-3" role="alert">
-	{{ $errorMessage }}
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" class="form-control" id="password" name="password" required>
+				<div class="invalid-feedback">
+					Password is required.
+				</div>
+			</div>
+
+			<a href="{{ url('/auth/reset-password') }}">Reset password</a>
+
+			<button type="submit" class="btn btn-primary">Log In</button>
+		</form>
+
+		@if($errorMessage !== null)
+		<p class="card-text alert alert-danger mt-3" role="alert">
+			{{ $errorMessage }}
+		</p>
+		@endif
+	</div>
 </div>
-@endif
-
-<form class="needs-validation d-flex flex-column gap-3" novalidate method="post">
-	@csrf
-	<div class="form-group">
-		<label for="username">Username</label>
-		<input type="text" class="form-control"
-					 id="username" name="username" required
-					 value="{{ $username }}">
-		<div class="invalid-feedback">
-			Username is required.
-		</div>
-	</div>
-
-	<div class="form-group">
-		<label for="password">Password</label>
-		<input type="password" class="form-control" id="password" name="password" required>
-		<div class="invalid-feedback">
-			Password is required.
-		</div>
-	</div>
-
-	<a href="{{ url('/auth/reset-password') }}">Reset password</a>
-
-	<button type="submit" class="btn btn-primary">Log In</button>
-</form>
 
 <script>
 (function() {
