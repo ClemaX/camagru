754750cf52d30ce97940abfca0813393a24154d9
Clément Hamada <clement.hamada@elysium-security.com>
2025-03-12 02:40:14 +0100

    fix: use error code 500 for internal server error

--- src/Exceptions/InternalException.php
+++ src/Exceptions/InternalException.php
@@ -8,8 +8,8 @@ class InternalException extends HttpException
 {
 	public function __construct(
 		string $message = "The server was not able to process the request",
-		int $code = 4090
+		int $code = 5000
 	) {
-		parent::__construct(409, "Internal Server Error", $message, $code);
+		parent::__construct(500, "Internal Server Error", $message, $code);
 	}
 }
