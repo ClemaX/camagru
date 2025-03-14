868f3041a5c35b302762c2abce4e08e297d403a6
Clément Hamada <clementhamada@protonmail.com>
2025-03-14 15:01:45 +0100

    feat(js): toggle collapse trigger class

--- js/collapse.js
+++ js/collapse.js
@@ -5,25 +5,32 @@ class Collapse {
 	}
 
 	getDimension() {
-		return this.element.classList.contains('collapse-horizontal')
-			? 'width' : 'height';
+		return this.element.classList.contains("collapse-horizontal")
+			? "width"
+			: "height";
 	}
 
 	queueAfterTransition(callback) {
 		const transitionDurationS = parseFloat(
-			getComputedStyle(this.element)['transitionDuration']);
+			getComputedStyle(this.element)["transitionDuration"]
+		);
 
 		const delayMs = transitionDurationS * 1000 + 5;
 
 		setTimeout(callback, delayMs);
 	}
 
+	/**
+	 * @returns {Promise<boolean>}
+	 */
 	show() {
-		if (this.isShown() || this.isTransitioning) return;
+		if (this.isShown() || this.isTransitioning) {
+			return true;
+		}
 
 		const dimension = this.getDimension();
-		const capitalizedDimension = dimension[0].toUpperCase()
-			+ dimension.slice(1);
+		const capitalizedDimension =
+			dimension[0].toUpperCase() + dimension.slice(1);
 
 		this.element.classList.remove("collapse");
 		this.element.classList.add("collapsing");
@@ -34,74 +41,107 @@ class Collapse {
 		this.queueAfterTransition(() => {
 			this.isTransitioning = false;
 
-			this.element.classList.remove('collapsing');
-			this.element.classList.add('collapse', 'show');
+			this.element.classList.remove("collapsing");
+			this.element.classList.add("collapse", "show");
 
-			this.element.style[dimension] = '';
+			this.element.style[dimension] = "";
 		});
 
 		const scrollSize = `scroll${capitalizedDimension}`;
 
 		this.element.style[dimension] = `${this.element[scrollSize]}px`;
+
+		return true;
 	}
 
+	/**
+	 * @returns {Promise<boolean>}
+	 */
 	hide() {
-		if (!this.isShown() || this.isTransitioning) return;
+		if (!this.isShown() || this.isTransitioning) {
+			return false;
+		}
 
 		const dimension = this.getDimension();
 
-		this.element.style[dimension] =
-			`${this.element.getBoundingClientRect()[dimension]}px`;
+		this.element.style[dimension] = `${
+			this.element.getBoundingClientRect()[dimension]
+		}px`;
 
 		// Reflow element
 		this.element.offsetHeight;
 
-		this.element.classList.add('collapsing');
-		this.element.classList.remove('collapse', 'show');
+		this.element.classList.add("collapsing");
+		this.element.classList.remove("collapse", "show");
 
 		this.isTransitioning = true;
 
-		this.element.style[dimension] = '';
+		this.element.style[dimension] = "";
 
 		this.queueAfterTransition(() => {
 			this.isTransitioning = false;
 
-			this.element.classList.remove('collapsing');
-			this.element.classList.add('collapse');
+			this.element.classList.remove("collapsing");
+			this.element.classList.add("collapse");
 		});
+
+		return false;
 	}
 
+	/**
+	 * @returns {Promise<boolean>}
+	 */
 	toggle() {
-		!this.isShown() ? this.show() : this.hide();
+		return !this.isShown() ? this.show() : this.hide();
 	}
 
 	isShown() {
-		return this.element.classList.contains('show');
+		return this.element.classList.contains("show");
 	}
 }
 
 const _collapses = {};
 
+/**
+ * @param {HTMLElement} target
+ * @returns {Collapse}
+ */
 const getOrCreateCollapse = (target) => {
 	if (!(target.id in _collapses)) {
 		_collapses[target.id] = new Collapse(target);
 	}
 
 	return _collapses[target.id];
-}
+};
 
 (() => {
-	'use strict';
-	document.addEventListener('DOMContentLoaded', () => {
+	"use strict";
+	/**
+	 *
+	 * @param {MouseEvent} e
+	 */
+	const handleTriggerClick = async (e) => {
+		/** @type {HTMLElement} */
+		const trigger = e.target;
+		const collapseId = trigger.getAttribute("data-bs-target");
+		const collapse = getOrCreateCollapse(document.querySelector(collapseId));
+
+		const isShown = await collapse.toggle();
+
+		if (isShown) {
+			trigger.classList.remove("collapsed");
+		} else {
+			trigger.classList.add("collapsed");
+		}
+	};
+
+	document.addEventListener("DOMContentLoaded", () => {
 		const collapseTriggers = document.querySelectorAll(
-			'[data-bs-toggle="collapse"]');
+			'[data-bs-toggle="collapse"]'
+		);
 
 		collapseTriggers.forEach((trigger) => {
-			const targetcollapseId = trigger.getAttribute('data-bs-target');
-
-			const collapse = getOrCreateCollapse(document.querySelector(targetcollapseId));
-
-			trigger.addEventListener('click', () => collapse.toggle());
+			trigger.addEventListener("click", handleTriggerClick);
 		});
 	});
 })();
