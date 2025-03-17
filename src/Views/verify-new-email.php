36ef6fc22a161de1eb5f99860d38d10d23e4009d
Clément Hamada <clement.hamada@elysium-security.com>
2025-03-17 18:05:07 +0100

    fix(view): add container and card to verify new email

--- src/Views/verify-new-email.php
+++ src/Views/verify-new-email.php
@@ -1,7 +1,15 @@
-<h1>Check your inbox</h1>
-<p>
-	A verification email has been sent with a link to change the account's email address to the address you have provided.
-</p>
-<p>
-	Please check your inbox and click on the verification link.
-</p>
+<div class="container">
+	<div class="card">
+		<div class="card-header">
+			<h1 class="card-title">Check your inbox</h1>
+		</div>
+		<div class="card-body">
+			<p>
+				A verification email has been sent with a link to change the account's email address to the address you have provided.
+			</p>
+			<p>
+				Please check your inbox and click on the verification link.
+			</p>
+		</div>
+	</div>
+</div>
